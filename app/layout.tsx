4cef0be41821b16921327a77979b86b4560ec53f
Mantissa <negativemantissa@gmail.com>
2026-03-21 05:27:05 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,21 @@
 import "./globals.css";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export const metadata = {
+  title: "Ritual | Meme Generator",
+  description: "Custom Web3 Character Meme Generator",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="en">
+      <head>
+        {/* Bypass the broken build pipeline and force Tailwind to run */}
+        <script src="https://cdn.tailwindcss.com"></script>
+      </head>
       <body>{children}</body>
     </html>
   );
